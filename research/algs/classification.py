b2dc61ccad3108d4feb00d352ee1c8358fa233f6
Joey Hejna <joey.hejna@gmail.com>
2021-12-25 11:29:18 -0800

    add working mnist sweep

--- research/algs/classification.py
+++ research/algs/classification.py
@@ -9,20 +9,32 @@ class Classification(Algorithm):
         self.criterion = torch.nn.CrossEntropyLoss()
 
     def _compute_loss_and_accuracy(self, batch):
+        '''
+        Helper function used to reduce repeated computation
+        '''
         x, y = batch
         yhat = self.network(x)
         loss = self.criterion(yhat, y)
+        import pdb; pdb.set_trace()
         with torch.no_grad():
-            accuracy = (torch.argmax(yhat) == y).sum() / y.shape[0]
+            accuracy = (torch.argmax(yhat, dim=-1) == y).sum() / y.shape[0]
         return loss, accuracy
 
     def _train_step(self, batch):
-        self.optim['network'].zero_grad()
+        '''
+        Overriding the Algorithm BaseClass Method _train_step.
+        Returns a dictionary of training metrics.
+        '''
+        self.optim['network'].zero_grad() # By default, the optimizer is stored under 'network' if no custom optimizer setup is performed.
         loss, accuracy = self._compute_loss_and_accuracy(batch)
         loss.backward()
         self.optim['network'].step()
         return dict(loss=loss.item(), accuracy=accuracy.item())
 
     def _validation_step(self, batch):
+        '''
+        Overriding the Algorithm BaseClass Method _train_step.
+        Returns a dictionary of validation metrics.
+        '''
         loss, accuracy = self._compute_loss_and_accuracy(batch)
         return dict(loss=loss.item(), accuracy=accuracy.item())
