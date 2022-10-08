update normalization to be all tensor and fix ppo

research/algs/ppo.py
@@ -47,7 +47,7 @@ class PPO(Algorithm):
         self.normalize_returns = normalize_returns
         self.reward_clip = reward_clip
         if self.normalize_returns:
-            self.return_rms = RunningMeanStd(shape=(), dtype=np.float64)
+            self.return_rms = RunningMeanStd(shape=())
 
         # Losses
         self.value_criterion = torch.nn.MSELoss()
@@ -62,13 +62,14 @@ class PPO(Algorithm):
         ep_reward, ep_length, ep_return, ep_success = 0, 0, 0, False
 
         obs = self.env.reset()
-        if isinstance(self.processor, RunningObservationNormalizer):
-            self.processor.update(obs)
         self.dataset.add(obs=obs)  # Add the first observation
         while not self.dataset.is_full:
             with torch.no_grad():
-                batch = self._format_batch(utils.unsqueeze(obs, 0))  # Preprocess obs
-                latent = self.network.encoder(batch)
+                obs = utils.unsqueeze(utils.to_tensor(obs), 0)
+                if isinstance(self.processor, RunningObservationNormalizer):
+                    self.processor.update(obs)
+                batch = self._format_batch(dict(obs=obs))  # Preprocess obs
+                latent = self.network.encoder(batch["obs"])
                 dist = self.network.actor(latent)
                 # Collect relevant information
                 action = dist.sample()
@@ -84,15 +85,13 @@ class PPO(Algorithm):
                 clipped_action = np.clip(action, self.action_space.low, self.action_space.high)
 
             obs, reward, done, info = self.env.step(clipped_action)
-            if isinstance(self.processor, RunningObservationNormalizer):
-                self.processor.update(obs)
 
             ep_reward += reward
             ep_length += 1
             ep_return = self.dataset.discount * ep_return + reward
             if self.normalize_returns:
                 self.return_rms.update(ep_return)
-                reward = reward / self.return_rms.std
+                reward = reward / self.return_rms.std.numpy()
                 if self.reward_clip is not None:
                     reward = np.clip(reward, -self.reward_clip, self.reward_clip)
 
@@ -107,8 +106,11 @@ class PPO(Algorithm):
                 ep_reward, ep_length, ep_return, ep_success = 0, 0, 0, False
                 # If its done, we need to update the observation as well as the terminal reward
                 with torch.no_grad():
-                    batch = self._format_batch(utils.unsqueeze(obs, 0))  # Preprocess obs
-                    terminal_value = self.network.critic(self.network.encoder(batch))
+                    obs = utils.unsqueeze(utils.to_tensor(obs), 0)
+                    if isinstance(self.processor, RunningObservationNormalizer):
+                        self.processor.update(obs)
+                    batch = self._format_batch(dict(obs=obs))  # Preprocess obs
+                    terminal_value = self.network.critic(self.network.encoder(batch["obs"]))
                     terminal_value = utils.to_np(utils.get_from_batch(terminal_value, 0))
                 reward += self.dataset.discount * terminal_value
                 obs = self.env.reset()
@@ -141,6 +143,7 @@ class PPO(Algorithm):
         if self.dataset.last_batch and self.epochs % self.num_epochs == 0:
             # On the last batch of the epoch recollect data.
             metrics.update(self._collect_rollouts())
+            return metrics  # Return immediatly so we don't do a gradient step on old data.
 
         # Run the policy to predict the values, log probs, and entropies
         latent = self.network.encoder(batch["obs"])

research/processors/concatenate.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List, Optional
 
 import gym
 import numpy as np
@@ -47,4 +47,42 @@ class ConcatenateProcessor(Processor):
             batch["obs"] = torch.cat([batch["obs"][k] for k in self.obs_order], dim=-1)
             if "next_obs" in batch:
                 batch["next_obs"] = torch.cat([batch["next_obs"][k] for k in self.obs_order], dim=-1)
+        return batch
+
+
+class SelectProcessor(Processor):
+    def __init__(
+        self,
+        observation_space: gym.Space,
+        action_space: gym.Space,
+        obs_include: Optional[List[str]] = None,
+        obs_exclude: Optional[List[str]] = None,
+        action_include: Optional[List[str]] = None,
+        action_exclude: Optional[List[str]] = None,
+    ):
+        super().__init__(observation_space, action_space)
+        assert not (action_include is not None and action_exclude is not None)
+        assert not (obs_include is not None and obs_exclude is not None)
+
+        if action_include is not None:
+            self.action_keys = [k for k in action_space.keys() if k in action_include]
+        elif action_exclude is not None:
+            self.action_keys = [k for k in action_space.keys() if k not in action_exclude]
+        else:
+            self.action_keys = None
+
+        if obs_include is not None:
+            self.obs_keys = [k for k in observation_space.keys() if k in obs_include]
+        elif obs_exclude is not None:
+            self.obs_keys = [k for k in observation_space.keys() if k not in obs_exclude]
+        else:
+            self.obs_keys = None
+
+    def forward(self, batch: Dict) -> Dict:
+        if "action" in batch and self.action_keys is not None:
+            batch["action"] = {k: batch["action"][k] for k in self.action_keys}
+        if "obs" in batch and self.obs_keys is not None:
+            batch["obs"] = {k: batch["obs"][k] for k in self.obs_keys}
+            if "next_obs" in batch:
+                batch["obs"] = {k: batch["obs"][k] for k in self.obs_keys}
         return batch

research/processors/normalization.py
@@ -1,4 +1,4 @@
-from multiprocessing.sharedctypes import Value
+from typing import Dict, Union
 
 import gym
 import numpy as np
@@ -10,29 +10,54 @@ from .base import Processor
 
 
 class RunningMeanStd(object):
-    def __init__(self, shape, dtype, epsilon=1e-6):
-        self._mean = np.zeros(shape, dtype=dtype)
-        self._m2 = np.ones(shape, dtype=dtype)
-        self.count = epsilon
+    def __init__(self, shape, epsilon: float = 1e-6):
+        self.shape = shape
+        self._mean = torch.zeros(shape, dtype=torch.float)
+        self._var = torch.ones(shape, dtype=torch.float)
+        self._count = epsilon
 
-    def update(self, x):
-        self.count += 1
-        delta = x - self._mean
-        self._mean = self._mean + delta / self.count
-        # Update the second moment
-        self._m2 = self._m2 + delta * (x - self._mean)
+    def update(self, x: Union[float, np.ndarray, torch.Tensor]) -> None:
+        if isinstance(x, float):
+            x = torch.tensor(x)
+        elif isinstance(x, np.ndarray):
+            x = torch.from_numpy(x)
+        elif isinstance(x, torch.Tensor):
+            pass
+        else:
+            raise ValueError("Invalid type provided")
+        # If the data is unbatched, unsqueeze it
+        if len(x.shape) == len(self.shape):
+            x = x.unsqueeze(0)
+
+        mean = torch.mean(x, dim=0)
+        var = torch.var(x, dim=0, unbiased=False)
+        count = x.shape[0]
+
+        delta = mean - self._mean
+        total_count = self._count + count
+
+        new_mean = self._mean + delta * count / total_count
+        m_a = self._var * self._count
+        m_b = var * count
+        m_2 = m_a + m_b + torch.square(delta) * self._count * count / total_count
+        new_var = m_2 / total_count
+
+        # Update member variables
+        self._count = total_count
+        self._mean = new_mean
+        self._var = new_var
 
     @property
     def mean(self):
-        return self._mean.astype(np.float32)
+        return self._mean
 
     @property
     def var(self):
-        return (self._m2 / self.count).astype(np.float32)
+        return self._var
 
     @property
     def std(self):
-        return np.sqrt(self.var)
+        return torch.sqrt(self._var)
 
 
 class RunningObservationNormalizer(Processor):
@@ -43,36 +68,62 @@ class RunningObservationNormalizer(Processor):
     2. We could permanently store torch tensors so we don't recompute them and sync to GPU.
     """
 
-    def __init__(self, observation_space, action_space, epsilon=1e-7, clip=10):
+    def __init__(
+        self,
+        observation_space: gym.Space,
+        action_space: gym.Space,
+        epsilon: float = 1e-7,
+        clip: float = 10,
+        explicit_update: bool = False,
+    ) -> None:
         super().__init__(observation_space, action_space)
-        assert isinstance(observation_space, gym.spaces.Box), "Currently only supports box spaces."
-        self.rms = RunningMeanStd(observation_space.shape, observation_space.dtype, epsilon)
+        if isinstance(observation_space, gym.spaces.Dict):
+            assert all([isinstance(space, gym.spaces.Box) for space in observation_space.values()])
+            self.rms = {k: RunningMeanStd(space.shape, epsilon=epsilon) for k, space in observation_space.items()}
+        elif isinstance(observation_space, gym.spaces.Box):
+            self.rms = RunningMeanStd(observation_space.shape, epsilon=epsilon)
+        else:
+            raise ValueError("Invalid space type provided.")
         self._updated_stats = True
         self.clip = clip
+        self.explicit_update = explicit_update
 
-    def update(self, obs: np.ndarray) -> None:
+    @property
+    def supports_gpu(self):
+        return False
+
+    def update(self, obs: Union[torch.Tensor, Dict]) -> None:
         self.rms.update(obs)
         self._updated_stats = True
 
-    def forward(self, batch):
+    def normalize(self, obs: Union[torch.Tensor, Dict]) -> Union[torch.Tensor, Dict]:
         if self._updated_stats:
-            # Update the tensors
-            device = utils.get_device(batch)
+            # Grab the states from the RMS trackers
+            self._mean = {k: self.rms[k].mean for k in self.rms.keys()} if isinstance(obs, dict) else self.rms.mean
+            self._std = {k: self.rms[k].std for k in self.rms.keys()} if isinstance(obs, dict) else self.rms.std
+            device = utils.get_device(obs)
             if device is not None:
-                self._mean_tensor = torch.from_numpy(self.rms.mean).to(device)
-                self._std_tensor = torch.from_numpy(self.rms.std).to(device)
+                self._mean = utils.to_device(self._mean, device)
+                self._std = utils.to_device(self._std, device)
             self._updated_stats = False
-        # Normalize by the input type
-        if isinstance(batch, dict):
-            for k in ("obs", "next_obs"):
-                if k in batch:
-                    batch[k] = self(batch[k])
-            return batch
-        if isinstance(batch, torch.Tensor):
-            batch = (batch - self._mean_tensor) / self._std_tensor
-            return torch.clamp(batch, -self.clip, self.clip) if self.clip is not None else batch
-        elif isinstance(batch, np.ndarray):
-            batch = (batch - self.rms.mean) / self.rms.std
-            return np.clip(batch, -self.clip, self.clip) if self.clip is not None else batch
+        # Normalize the observation
+        if isinstance(obs, dict):
+            for k in obs.keys():
+                obs[k] = (obs[k] - self._mean[k]) / self._std[k]
+                if self.clip is not None:
+                    obs[k] = torch.clamp(obs[k], -self.clip, self.clip)
+        elif isinstance(obs, torch.Tensor):
+            obs = (obs - self._mean) / self._std
+            return obs if self.clip is None else torch.clamp(obs, -self.clip, self.clip)
         else:
-            raise ValueError("Invalid input provided to ObservationNormalizer")
+            raise ValueError("Invalid Input provided")
+
+    def forward(self, batch):
+        # Check if we should update the statistics
+        if not self.explicit_update and self.training and "obs" in batch:
+            self.update(batch["obs"])
+        # Normalize
+        batch["obs"] = self.normalize(batch["obs"])
+        if "next_obs" in batch:
+            batch["next_obs"] = self.normalize["next_obs"]
+        return batch