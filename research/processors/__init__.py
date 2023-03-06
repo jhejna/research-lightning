4d9bb90b095249d34a875dd35cb70d8e68d24894
Joey Hejna <joey.hejna@gmail.com>
2023-03-05 17:41:24 -0800

    update image aug to be more versitile

--- research/processors/__init__.py
+++ research/processors/__init__.py
@@ -1,5 +1,5 @@
 # Register Preprocessors here
 from .base import ComposeProcessor
-from .image_augmentation import RandomShiftsAug
+from .image_augmentation import RandomCrop
 from .normalization import RunningObservationNormalizer
 from .concatenate import ConcatenateProcessor
